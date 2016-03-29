a8c5d6082b1d5bae40af4a8f271a8821076d201b
Andrzej1991 <andrzej.perski@hotmail.com>
2016-03-29 17:24:46 +0200

    Initial commit

---
+++ app/src/main/java/com/company/andrzej/superman/BotBorder.java
@@ -0,0 +1,32 @@
+package com.company.andrzej.superman;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by Andrzej on 2016-01-25.
+ */
+public class BotBorder extends GameObject {
+
+    private Bitmap image;
+    public  BotBorder(Bitmap res, int x, int y)
+    {
+        height = 200;
+        width = 20;
+
+        this.x = x;
+        this.y = y;
+        dx = GamePanel.MOVESPEED;
+
+        image = Bitmap.createBitmap(res,0,0, width, height);
+    }
+    public void update()
+    {
+        x += dx;
+    }
+    public void draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image,x,y,null);
+    }
+
+}
---
+++ app/src/main/java/com/company/andrzej/superman/Smokepuff.java
@@ -0,0 +1,31 @@
+package com.company.andrzej.superman;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by Andrzej on 2016-01-22.
+ */
+public class Smokepuff extends GameObject {
+    public int r;
+    public Smokepuff(int x, int y)
+    {
+        r = 5;
+        super.x = x;
+        super.y = y;
+    }
+    public void update()
+    {
+        x-=10;
+    }
+    public void draw(Canvas canvas)
+    {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        canvas.drawLine(x-r, y-r, r, y, paint);
+
+    }
+}
